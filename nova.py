c4348f967fdfeacbf11c7a708b6eead1f4b420ae
Mikl T <taraskin@uchi.ru>
2016-03-22 12:24:07 +0300

    Changed all sensitive data to env variables, fixed bug in flavour creation, finished automatic quotes management, tested with server creation

--- nova.py
+++ nova.py
@@ -2,6 +2,8 @@ import os
 import inspect
 import time
 import argparse
+import httplib
+import json
 from keystoneauth1.identity import v3
 from keystoneauth1 import loading
 from keystoneauth1 import session
@@ -15,17 +17,19 @@ parser.add_argument('--disk', dest='disk', default=10, type=int, help='Disk spac
 
 args = parser.parse_args()
 
-RAM=args.ram
-VCPU=args.vcpu
-DISK=args.disk
+SERVERS = args.server_names
+SERVERS_COUNT = len(SERVERS)
+RAM = args.ram
+VCPU = args.vcpu
+DISK = args.disk
 
-AUTH_URL=os.environ['AUTH_URL']
-VERSION="2"
-PROJECT_DOMAIN_NAME = os.environ['PROJECT_DOMAIN_NAME']
-PROJECT_ID = os.environ['SELECTEL_TARGET_PROJECT']
-PASSWORD = os.environ['SELECTEL_PASSWORD']
-DOMAIN_NAME = os.environ['DOMAIN_NAME']
-USERNAME = os.environ['USERNAME']
+AUTH_URL=os.environ['OS_AUTH_URL']
+VERSION=os.environ['OS_VOLUME_API_VERSION']
+PROJECT_DOMAIN_NAME = os.environ['OS_PROJECT_DOMAIN_NAME']
+PROJECT_ID = os.environ['OS_PROJECT_ID']
+PASSWORD = os.environ['OS_PASSWORD']
+DOMAIN_NAME = os.environ['OS_USER_DOMAIN_NAME']
+USERNAME = os.environ['OS_USERNAME']
 
 auth = v3.Password( auth_url=AUTH_URL, username=USERNAME, password=PASSWORD, user_domain_name=DOMAIN_NAME, project_domain_name=PROJECT_DOMAIN_NAME, project_id=PROJECT_ID)
 sess = session.Session(auth=auth)
@@ -36,29 +40,28 @@ IMAGE_WITH_RUBY = '061b63e6-e203-4247-a628-1b2a1d632fa8'
 INTERNAL_NETWORK_ID='3fd2b832-5849-46dc-a03f-070f852e43ee'
 
 SELECTEL_HOST = "api.selectel.ru"
-SELECTEL_API_TOKEN = os.environ['SELECTEL_API_TOKEN']
+SELECTEL_API_TOKEN = os.environ['OS_SELECTEL_TOKEN']
 
 def manage_qoutas(prject_id, ram, vcpu, disk):
-  free_qoutas = compute_free_quotas(get_project_quotas(prject_id))
-  ram =  None if free_qoutas['compute_ram'] >= vcpu else ram - free_qoutas['compute_ram']
-  vcpu = None if free_qoutas['compute_cores'] >= vcpu else vcpu - free_qoutas['compute_cores']
-  base_disk = None if free_qoutas['volume_gigabytes_basic'] >= vcpu else disk - free_qoutas['volume_gigabytes_basic']
-  increase_quotas(prject_id, ram, vcpu, base_disk)
+  quotas = get_project_quotas(prject_id)
+  free_qoutas = compute_free_quotas(quotas)
+  ram =  None if free_qoutas['compute_ram'] >= vcpu else quotas['project']['quotas_usage']['compute_ram'] + (ram - free_qoutas['compute_ram'])
+  vcpu = None if free_qoutas['compute_cores'] >= vcpu else quotas['project']['quotas_usage']['compute_cores'] + (vcpu - free_qoutas['compute_cores'])
+  base_disk = None if free_qoutas['volume_gigabytes_basic'] >= vcpu else quotas['project']['quotas_usage']['volume_gigabytes_basic'] + (disk - free_qoutas['volume_gigabytes_basic'])
+  return set_quotas(prject_id, ram, vcpu, base_disk)
 
-def increase_quotas(prject_id, ram=None, vcpu=None, base_disk=None):
+def set_quotas(prject_id, ram=None, vcpu=None, base_disk=None):
   conn = httplib.HTTPSConnection(SELECTEL_HOST)
   quotas_hash = {}
-
-  if vcpu is not None:
+  if int(vcpu or 0) != 0:
     quotas_hash['compute_cores'] = vcpu
-  if vcpu is not None:
+  if int(ram or 0) != 0:
     quotas_hash['compute_ram'] = ram
-  if base_disk is not None:
+  if int(base_disk or 0) != 0:
     quotas_hash['volume_gigabytes_basic'] = base_disk
-
-  conn.request("PATCH", "/vpc/resell/v1/quotas/projects/".format(prject_id), json.dumps({ 'quotas': quotas_hash }), { 'X-token': SELECTEL_API_TOKEN, 'Content-Type' : 'application/json' })
+  conn.request("PATCH", "/vpc/resell/v1/quotas/projects/{0}".format(prject_id), json.dumps({ 'quotas': quotas_hash }), { 'X-token': SELECTEL_API_TOKEN, 'Content-Type' : 'application/json' })
   response = conn.getresponse()
-  return json.loads(response.read())
+  return response.read()
 
 def compute_free_quotas(project_quotas):
   project_attrs = project_quotas['project']
@@ -73,12 +76,11 @@ def get_project_quotas(project_id):
   response = conn.getresponse()
   return json.loads(response.read())
 
-manage_qoutas(PROJECT_ID, RAM, VCPU, DISK)
-
+manage_qoutas(PROJECT_ID, RAM * SERVERS_COUNT, VCPU * SERVERS_COUNT, DISK * SERVERS_COUNT)
 
 existing_flavors = nova.flavors.findall(ram=RAM,vcpus=VCPU, disk=0)
-flavor = existing_flavors[0] if len(existing_flavors) >= 1 else nova.flavors.create(ram=RAM,vcpus=VCPU, disk=0)
+flavor = existing_flavors[0] if len(existing_flavors) >= 1 else nova.flavors.create(name=SERVERS[0],ram=RAM,vcpus=VCPU, disk=0)
 
-for server_name in args.server_names:
-  block_device_mapping_v2=[{"boot_index": 0, "destination_type": "volume", "source_type": "image", "uuid": IMAGE_WITH_RUBY, "device_name": "vda", 'volume_type': "basic", "volume_size": DISK, 'volume_name': "disk-for-{server_name}" }]
+for server_name in SERVERS:
+  block_device_mapping_v2=[{"boot_index": 0, "destination_type": "volume", "source_type": "image", "uuid": IMAGE_WITH_RUBY, "device_name": "vda", 'volume_type': "basic", "volume_size": DISK, 'volume_name': "disk-for-{0}".format(server_name) }]
   server = nova.servers.create( name=server_name, image=None, block_device_mapping_v2=block_device_mapping_v2, min_count=1, max_count=1, config_drive=True, key_name='mik_key', flavor=flavor.id, nics=[{ 'net-id': INTERNAL_NETWORK_ID }])
